49fa32bd82998d09b3f52b53fc2c562f417c1478
Sagi Shnaidman <sshnaidm@redhat.com>
2017-05-11 01:27:28 +0300

    Cleanup conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,3 @@
 import os
-import pytest
 import sys
-import tempfile
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
-from testex.app import app
-
-
-@pytest.fixture
-def client(request):
-    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
-    app.config['TESTING'] = True
-    client = app.test_client()
-
-    def teardown():
-        os.close(db_fd)
-        os.unlink(app.config['DATABASE'])
-    request.addfinalizer(teardown)
-    return client
